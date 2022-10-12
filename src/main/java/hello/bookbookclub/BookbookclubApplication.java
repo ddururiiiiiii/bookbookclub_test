fac9c36b4d42142574f8a92efa93d721c828fd98
ddururiiiiiii <o244227@daum.net>
2022-10-12 22:05:08 +0900

    토이프로젝트 시작해요!

---
+++ src/main/java/hello/bookbookclub/BookbookclubApplication.java
@@ -0,0 +1,13 @@
+package hello.bookbookclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookbookclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookbookclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/bookbookclub/BookbookclubApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.bookbookclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookbookclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
